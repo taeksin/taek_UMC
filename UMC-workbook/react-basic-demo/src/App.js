2d8315d61f77585e0336e62f96f00b328ca99e5f
taeksin <yoyo2521@naver.com>
2023-10-04 16:51:41 +0900

    note

---
+++ UMC-workbook/react-basic-demo/src/App.js
@@ -0,0 +1,22 @@
+import { movies } from './movieDummy';
+import Movie from './components/Movie';
+
+function App() {
+  return (
+    <div>
+      <div className="app-container">
+        {movies.results.map((item) => {
+          return (
+            <Movie
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
